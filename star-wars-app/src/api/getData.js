68c136304c8fe08fbeb59eeadb60994c9ebcdf9c
Tristan Depew <tristan.depew@gmail.com>
2020-01-27 20:12:06 -0500

    finished mvp

--- star-wars-app/src/api/getData.js
+++ star-wars-app/src/api/getData.js
@@ -2,6 +2,7 @@ import axios from "axios";
 export const getData = async url => {
   try {
     const res = await axios.get(url);
+    console.log(res);
     return res.data;
   } catch (err) {
     throw new Error(err);
